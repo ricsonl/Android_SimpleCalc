c268ef60679056b86d97300ee2d8360db0dd9410
Ricson Luiz <ricsonluizv@gmail.com>
2020-09-23 20:20:39 -0300

    completeCalc add to output expression & expression result

--- app/src/main/java/android/teste/simplecalc/CompleteCalcActivity.java
+++ app/src/main/java/android/teste/simplecalc/CompleteCalcActivity.java
@@ -2,6 +2,10 @@ package android.teste.simplecalc;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import org.mariuszgromada.math.mxparser.*;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -14,7 +18,27 @@ public class CompleteCalcActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
+    public void changeExpression(View v){
+        EditText output = findViewById(R.id.visor);
+        String outTxt = output.getText().toString();
+        String pressedTag = v.getTag().toString();
+        String buttonText = ((Button)v).getText().toString();
+        if(pressedTag.equals("Back")){
+            if(outTxt.substring(outTxt.length() - 1).equals(" "))
+                output.setText(outTxt.substring(0, outTxt.length()-2));
+            else output.setText(outTxt.substring(0, outTxt.length()-1));
+        } else if(pressedTag.equals("Clear")){
+            output.setText("");
+        } else output.setText(outTxt + buttonText);
+    }
 
+    public void calculateExpression(View v){
+        EditText output = findViewById(R.id.visor);
+        String outTxt = output.getText().toString().replaceAll("\\s+", "");
+        Expression e = new Expression(outTxt);
+        String result = String.valueOf(e.calculate());
+        output.setText(result);
+    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
