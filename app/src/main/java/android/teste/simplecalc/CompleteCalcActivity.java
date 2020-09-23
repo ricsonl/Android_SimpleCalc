b46ceaefd1f4b688e1f42a1511b9fba017ad2c71
Ricson Luiz <ricsonluizv@gmail.com>
2020-09-23 11:54:35 -0300

    back button

--- app/src/main/java/android/teste/simplecalc/CompleteCalcActivity.java
+++ app/src/main/java/android/teste/simplecalc/CompleteCalcActivity.java
@@ -1,6 +1,7 @@
 package android.teste.simplecalc;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,5 +10,17 @@ public class CompleteCalcActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_complete);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id==android.R.id.home) {
+            finish();
+        }
+        return true;
     }
 }
--- app/src/main/java/android/teste/simplecalc/SimpleCalcActivity.java
+++ app/src/main/java/android/teste/simplecalc/SimpleCalcActivity.java
@@ -1,6 +1,9 @@
 package android.teste.simplecalc;
 
+import android.app.ActionBar;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,5 +12,17 @@ public class SimpleCalcActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_simple);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id==android.R.id.home) {
+            finish();
+        }
+        return true;
     }
 }
