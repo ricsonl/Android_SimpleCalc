080f86a79ab850945473a11d1453b8c16318a1d4
Ricson Luiz <ricsonluizv@gmail.com>
2020-09-23 10:42:03 -0300

    activity navigation

---
+++ app/src/main/java/android/teste/simplecalc/CompleteCalcActivity.java
@@ -0,0 +1,13 @@
+package android.teste.simplecalc;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CompleteCalcActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_complete);
+    }
+}
--- app/src/main/java/android/teste/simplecalc/MainActivity.java
+++ app/src/main/java/android/teste/simplecalc/MainActivity.java
@@ -2,6 +2,8 @@ package android.teste.simplecalc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.view.View;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void goToCalc(View v){
+        String tag = v.getTag().toString();
+        System.out.println(tag);
+        if(tag.equals("simple")){
+            Intent it = new Intent(getBaseContext(), SimpleCalcActivity.class);
+            startActivity(it);
+        } else if(tag.equals("complete")){
+            Intent it = new Intent(getBaseContext(), CompleteCalcActivity.class);
+            startActivity(it);
+        }
     }
 }
